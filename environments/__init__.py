30f1d39aef2b412e99b83740a9d7550fd9821ce5
mmallo3 <mmallo3@gatech.edu>
2019-04-10 14:41:35 -0400

    Fixed rewards not being properly used

--- environments/__init__.py
+++ environments/__init__.py
@@ -17,216 +17,113 @@ __all__ = ['RewardingFrozenLakeEnv', 'WindyCliffWalkingEnv']
 register(
     id='RewardingFrozenLake4x4-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '4x4', 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='RewardingFrozenLake8x8-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '8x8', 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='RewardingFrozenLake15x15-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '15x15', 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='RewardingFrozenLake20x20-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '20x20', 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='RewardingFrozenLakeNoRewards4x4-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '4x4', 'rewarding': False, 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='RewardingFrozenLakeNoRewards8x8-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '8x8', 'rewarding': False, 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='RewardingFrozenLakeNoRewards15x15-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '15x15', 'rewarding': False, 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='RewardingFrozenLakeNoRewards20x20-v0',
     entry_point='environments:RewardingFrozenLakeEnv',
-    kwargs={'map_name': '20x20', 'rewarding': False, 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW},
 )
 
 register(
     id='CliffWalking4x12-v0',
     entry_point='environments:WindyCliffWalkingEnv',
-    kwargs={'wind_prob': 0, 'step_rew': CLIFF_STEP_REW, 'fall_rew': CLIFF_FALL_REW, 'goal_rew': CLIFF_GOAL_REW},
 )
 
 register(
     id='WindyCliffWalking4x12-v0',
     entry_point='environments:WindyCliffWalkingEnv',
-    kwargs={'step_rew': CLIFF_STEP_REW, 'fall_rew': CLIFF_FALL_REW, 'goal_rew': CLIFF_GOAL_REW},
 )
 
 
 def get_rewarding_frozen_lake_4x4_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLake4x4-v0')
+    kwargs={'map_name': '4x4', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLake4x4-v0', **kwargs)
 
 
 def get_rewarding_frozen_lake_8x8_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLake8x8-v0')
+    kwargs={'map_name': '8x8', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLake8x8-v0', **kwargs)
 
 
 def get_large_rewarding_frozen_lake_12x12_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLake12x12-v0')
+    kwargs={'map_name': '12x12', 'step_rew': LAKE_STEP_REW, 'hole_rew': LAKE_HOLE_REW, 'goal_rew': LAKE_GOAL_REW}
+    return gym.make('RewardingFrozenLake12x12-v0', **kwargs)
 
 
 def get_large_rewarding_frozen_lake_15x15_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLake15x15-v0')
+    kwargs={'map_name': '15x15', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLake15x15-v0', **kwargs)
 
 
 def get_large_rewarding_frozen_lake_20x20_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLake20x20-v0')
+    kwargs={'map_name': '20x20', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLake20x20-v0', **kwargs)
 
 
 def get_frozen_lake_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
     return gym.make('FrozenLake-v0')
 
 
 def get_rewarding_no_reward_frozen_lake_4x4_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLakeNoRewards4x4-v0')
+    kwargs={'map_name': '4x4', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLakeNoRewards4x4-v0', **kwargs)
 
 
 def get_rewarding_no_reward_frozen_lake_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLakeNoRewards8x8-v0')
+    kwargs={'map_name': '8x8', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLakeNoRewards8x8-v0', **kwargs)
 
 
 def get_large_rewarding_no_reward_frozen_lake_12x12_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLakeNoRewards12x12-v0')
+    kwargs={'map_name': '12x12', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLakeNoRewards12x12-v0', **kwargs)
 
 
 def get_large_rewarding_no_reward_frozen_lake_15x15_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLakeNoRewards15x15-v0')
+    kwargs={'map_name': '15x15', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLakeNoRewards15x15-v0', **kwargs)
 
 
 def get_large_rewarding_no_reward_frozen_lake_20x20_environment(step_rew = LAKE_STEP_REW, hole_rew = LAKE_HOLE_REW, goal_rew = LAKE_GOAL_REW):
-
-    global LAKE_STEP_REW
-    global LAKE_HOLE_REW
-    global LAKE_GOAL_REW
-    LAKE_STEP_REW = step_rew
-    LAKE_HOLE_REW = hole_rew
-    LAKE_GOAL_REW = goal_rew
-
-    return gym.make('RewardingFrozenLakeNoRewards20x20-v0')
+    kwargs={'map_name': '20x20', 'step_rew': step_rew, 'hole_rew': hole_rew, 'goal_rew': goal_rew}
+    return gym.make('RewardingFrozenLakeNoRewards20x20-v0', **kwargs)
 
 
 def get_cliff_walking_4x12_environment(step_rew = CLIFF_STEP_REW, fall_rew = CLIFF_FALL_REW, goal_rew = CLIFF_GOAL_REW):
-
-    global CLIFF_STEP_REW
-    global CLIFF_FALL_REW
-    global CLIFF_GOAL_REW
-    CLIFF_STEP_REW = step_rew
-    CLIFF_FALL_REW = fall_rew
-    CLIFF_GOAL_REW = goal_rew
-
-    return gym.make('CliffWalking4x12-v0')
+    kwargs={'wind_prob': 0, 'step_rew': step_rew, 'fall_rew': fall_rew, 'goal_rew': goal_rew},
+    return gym.make('CliffWalking4x12-v0', **kwargs)
 
 
 def get_windy_cliff_walking_4x12_environment(step_rew = CLIFF_STEP_REW, fall_rew = CLIFF_FALL_REW, goal_rew = CLIFF_GOAL_REW):
-
-    global CLIFF_STEP_REW
-    global CLIFF_FALL_REW
-    global CLIFF_GOAL_REW
-    CLIFF_STEP_REW = step_rew
-    CLIFF_FALL_REW = fall_rew
-    CLIFF_GOAL_REW = goal_rew
-
     return gym.make('WindyCliffWalking4x12-v0')
 
